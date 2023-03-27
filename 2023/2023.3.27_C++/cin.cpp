117da970ce97c4b75e470ac093ee3923185f5f8c
H4cker7274 <110335446+NTremake@users.noreply.github.com>
2023-03-27 11:00:52 +0800

    Add files via upload

---
+++ 2023/2023.3.27_C++/cin.cpp
@@ -0,0 +1,8 @@
+int main()//数据的输入
+{
+	int a = 1;
+	cout << "请输入a的值：";
+	cin >> a;
+	cout << "a=" << a << endl;
+	return 0;
+}
---
+++ 2023/2023.3.27_C++/float.cpp
@@ -0,0 +1,7 @@
+int main()//字符串
+{
+	char str1[] = "abcd";//C语言风格的字符串
+	string str2 = "abcd";//C++风格的字符串，需要头文件<string>
+
+	return 0;
+}
