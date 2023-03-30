bf30cbf13c384450746dd6974968cecbf118b125
H4cker7274 <110335446+NTremake@users.noreply.github.com>
2023-03-30 12:37:58 +0800

    Delete 2023/2023.3.18_C directory

--- 2023/2023.3.18_C/HelloWorld.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>//standard input output.head
-
-int main()//只能有一个主函数(main)
-{
-  printf("Hellow World\n");//print function
-  printf("Hellow World\n");//  \n是换行的意思；\是转义字符，后面加不同的字符，有不同的效果;\ddd,是三个八进制数字，换成10进制看ASCII表；\xdd是x加两个十六进制数字
-  printf("Hellow World\n");
-  return 0;
-  }
--- 2023/2023.3.18_C/const p.c
+++
@@ -1,9 +0,0 @@
-int main()
-{
-	const int num1 = 1;
-	//num1 = 2;错误，不能被修改
-	int num2 = 2;
-	const int* p = &num2;
-	//*p = 1;错误，不能被修改
-	return 0;
-}
